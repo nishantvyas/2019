344dc2c95c3c9e848ca958815fa17a69ac2b0995
nishantvyas <nishantvyas@gmail.com>
2019-01-25 23:04:01 -0800

    set

--- list_tuple_set.py
+++ list_tuple_set.py
@@ -12,7 +12,9 @@ def my_tuple():
 
 def my_set():
     my_set = {1,2}
-    print(my_set)
+    print(my_set.union({3,4}))
+    print(my_set.intersection({2}))
+
 
 if __name__ == '__main__':
     my_list()
