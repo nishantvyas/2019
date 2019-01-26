cbf7d388c634e8fdf5e7f66936210d4d25d3e440
nishantvyas <nishantvyas@gmail.com>
2019-01-25 22:59:16 -0800

    tuple

--- list_tuple_set.py
+++ list_tuple_set.py
@@ -6,5 +6,11 @@ def my_list():
     my_list = [1,2,3]
     print(my_list)
 
+def my_tuple():
+    my_tuple = (1,2,3)
+    print(my_tuple)
+
+
 if __name__ == '__main__':
     my_list()
+    my_tuple()
