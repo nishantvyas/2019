44230f13e877431263b8406231d9029f7a7743f0
Nishant Vyas <nishantvyas@gmail.com>
2019-01-31 16:40:27 -0800

    function decorator

--- deco_simple.py
+++ deco_simple.py
@@ -13,9 +13,26 @@ def decorate_function(function):
 def my_function():
     print("This is what my_function does")
 
+def decorate_function_with_args(argument):
+    def decorate_function_with_args_func(function):
+        @functools.wraps(function)
+        def function_to_be_wrapped_with_args(*args, **kwargs):
+            print("Decorating the function here. Before.")
+            if argument == 1:
+                function(*args, **kwargs)
+            else:
+                pass
+            print("Decorating the function here. After.")
+        return function_to_be_wrapped_with_args
+    return decorate_function_with_args_func
+
+@decorate_function_with_args(1)
+def my_function_with_args(x, y):
+    print(x+y)
+
 def main():
     my_function()
-
+    my_function_with_args(100, 1)
 
 if __name__ == '__main__':
     main()
